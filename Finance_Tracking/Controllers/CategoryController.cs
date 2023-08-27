6863aef034204ef643cd2366b6ec942580701459
Ziiko97 <ziadeljerbi97@icloud.com>
2023-08-28 01:02:13 +0200

    Category and transaction

--- Finance_Tracking/Controllers/CategoryController.cs
+++ Finance_Tracking/Controllers/CategoryController.cs
@@ -26,24 +26,6 @@ namespace Finance_Tracking.Controllers
                           Problem("Entity set 'ApplicationDBContext.Categorys'  is null.");
         }
 
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Categorys == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categorys
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
         // GET: Category/AddOrEdit
         public IActionResult AddOrEdit(int id = 0)
         {
@@ -73,23 +55,6 @@ namespace Finance_Tracking.Controllers
         }
 
    
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Categorys == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categorys
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
 
         // POST: Category/Delete/5
         [HttpPost, ActionName("Delete")]
@@ -110,9 +75,5 @@ namespace Finance_Tracking.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CategoryExists(int id)
-        {
-          return (_context.Categorys?.Any(e => e.CategoryId == id)).GetValueOrDefault();
-        }
     }
 }
